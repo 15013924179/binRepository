29e8098fbf24c65d2538fb2a4f61c0079ab6ddc5
15013924179 <49760767+15013924179@users.noreply.github.com>
2020-11-26 22:39:28 +0800

    111

---
+++ meishikecan/src/test/java/com/bin/meishikecan/DownloadTest.java
@@ -0,0 +1,27 @@
+package com.bin.meishikecan;
+
+import com.bin.meishikecan.common.DownloadVedioTest;
+import com.bin.meishikecan.utils.DownloadVedioBySelenium;
+import com.bin.meishikecan.utils.DownloadVedioBySelenium1;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DownloadTest {
+
+
+
+    @Test
+    public void test() throws Exception{
+
+
+    }
+
+
+
+
+
+
+
+}
